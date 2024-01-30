3cf1abe350acd75e9c84da92f8ae8930bf40e91c
DeltaDizzy <EJBraswell@gmail.com>
2024-01-30 11:02:09 -0500

    move climber to subsystems

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,9 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.subsystems;
 
-import com.ctre.phoenix6.controls.DifferentialVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
